53e031949b157bdc8d7538ca1dac381c8deeba6a
gushijz <gus_sy@sina.com>
2017-04-05 11:08:22 +0800

    Initial Commit

---
+++ git练习/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git练习
+//
+//  Created by gushi on 2017/4/5.
+//  Copyright © 2017年 gushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
