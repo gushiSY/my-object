f3fd46ac8df3c9b3b4172fee0096215255c0f563
gushijz <gus_sy@sina.com>
2017-04-05 11:35:54 +0800

    22222

--- git练习/ViewController.swift
+++ git练习/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("新的")
+        print("dddddd")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
